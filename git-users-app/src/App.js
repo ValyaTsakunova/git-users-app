5681ceb5377c78633896d59b46e77bb5d1ad66ce
Valya Tsakunova <vtsakunova@mail.ru>
2021-05-13 00:33:43 +0300

    create search form

---
+++ git-users-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SearchForm from './components/SearchForm';
+import './assets/styles/App.css';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <SearchForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ git-users-app/src/components/SearchForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../assets/styles/SearchForm.css'
+
+function SearchForm() {
+    return(
+        <header>
+        <form>
+            <input type="text" placeholder="type here..."></input>
+        </form>
+        </header>
+        
+    )
+}
+
+export default SearchForm;
